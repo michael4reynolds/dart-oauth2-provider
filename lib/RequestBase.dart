a2cf73499e7eab1e764b5cc2e2a62693ed7aa33f
Michael Reynolds <basheps@gmail.com>
2014-02-03 18:24:06 -0500

    complete RequestBase

--- lib/RequestBase.dart
+++ lib/RequestBase.dart
@@ -3,13 +3,24 @@ library dartoauth2.provider;
 import 'OAuthException.dart';
 
 class RequestBase {
+  final Map<String, String> _headers;
+  final Map<String, List<String>>_params;
 
-  RequestBase(Map<String, String> headers, Map<String, String> params) {
-    String header(String name) => headers[name];
-    String requireHeader(String name) {
-     if( headers[name] != null)
-       return headers[name];
-    }
+  RequestBase(this._headers, this._params);
+
+  String header(String name) => _headers[name];
+
+  String requireHeader(String name) {
+    if (_headers[name] != null)
+      return _headers[name];
     throw new InvalidRequest("required header: $name");
+  }
+
+  String param(String name) => _params[name] != null ? _params[name].first : null;
+
+  String requireParam(String name) {
+    if (param(name) != null)
+      return param(name);
+    throw new InvalidRequest("required parameter: $name");
   }
 }
